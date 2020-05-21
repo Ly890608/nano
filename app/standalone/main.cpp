5ce2bc95e21dfe2334ad3593eac0b360d4214d2a
ly248342 <ly248342@alibaba-inc.com>
2020-05-21 18:41:36 +0800

    update

--- app/standalone/main.cpp
+++ app/standalone/main.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    const char *version = nano_version();
     const char *module_name = nano_module_name();
-    std::cout << version << std::endl;
-    std::cout << module_name << std::endl;
+    const char *version = nano_version();
+    std::cout << "module_name" << module_name << std::endl;
+    std::cout << "version" << version << std::endl;
     return 0;
 }
